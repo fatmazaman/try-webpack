218482d70d654d4f6d575b49ebf89e1f48b4b3c1
Fatma Zaman <fatma302@gmail.com>
2017-01-23 09:21:28 -0500

    babel -loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ const config = {
   output: {
     path: path.resolve(__dirname,'build'),
     filename: 'bundle.js'
+  },
+  module:{
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+
+    ]
   }
 };
 module.exports = config;
