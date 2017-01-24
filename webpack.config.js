f7320eb45551a68f362571c1a5a5b151d94e3c08
Fatma Zaman <fatma302@gmail.com>
2017-01-24 10:25:13 -0500

    image loader working with public path

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
 const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require ('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname,'build'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: 'build/'
   },
-  module:{
+  module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/
       },
       {
-        // use:['style-loader', 'css-loader'], loader and use are identical in webpack
         loader: ExtractTextPlugin.extract({
-          loader:'css-loader'
+          loader: 'css-loader'
         }),
-        test:/\.css$/
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
-
     ]
   },
-  plugins:[
+  plugins: [
     new ExtractTextPlugin('style.css')
   ]
 };
+
 module.exports = config;
